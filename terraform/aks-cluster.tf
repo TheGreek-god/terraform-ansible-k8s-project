d36196d49fc034c7017a6ada498b7c467b85fcf0
unknown <reachfinbarr@gmailm>
2025-08-29 18:15:02 +0000

    Initial commit

---
+++ terraform/aks-cluster.tf
@@ -0,0 +1,25 @@
+resource "azurerm_kubernetes_cluster" "aks-cluster" {
+  name                = terraform-cluster
+  location            = data.azurerm_resource_group.greekrg.location
+  resource_group_name = data.azurerm_resource_group.greekrg.name
+
+  dns_prefix          = "${data.azurerm_resource_group.greekrg.name}-dns"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1 # Disable default node pool by setting 0 nodes
+    vm_size    = "Standard_D2_v2"
+  }
+
+}
+
+resource "azurerm_kubernetes_cluster_node_pool" "nodepool" {
+  name                  = "intenal"
+  kubernetes_cluster_id = azurerm_kubernetes_cluster.example.id
+  vm_size               = "Standard_D2_v2" 
+  node_count            = 1  # min node count (initial)
+  tags = {
+    Environment = "Development"
+  }
+}
+
